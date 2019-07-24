12af5aed5d18c9eb1da710e5517ba318cbe62b79
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-07-24 16:13:27 -0700

    Add fix for card back

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -6,36 +6,65 @@ class Blog extends Component {
   render() {
     return (
       <div>
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <img
-                className="card-img-top"
-                src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
-                alt="bla"
-              />
-
-              <div className="card-body">
-                <h5 className="card-title">{this.props.blog.title}</h5>
-                <h6 className="font-italic">{this.props.blog.autor}</h6>
-                <p className="card-text">{this.props.blog.about}</p>
+        <div className="card-container">
+          <div className="card">
+            <div className="front">
+              <div className="cover">
+                <img
+                  className="card-image"
+                  src="https://www.roboticsbusinessreview.com/wp-content/uploads/2018/09/Fusion_world-1000x500.jpg"
+                  alt="cover"
+                />
+              </div>
+              <div className="user">
+                <img
+                  className="img-circle"
+                  src="https://stagerightshow.com/wp-content/uploads/2019/05/unique-halloween-black-cat-clipart-halloween-black-cat-png-transparent-this-year.png"
+                />
+              </div>
+              <div className="content">
+                <div className="main">
+                  <h3 className="name">{this.props.blog.autor}</h3>
+                  <p className="profession">{this.props.blog.title}</p>
+                  <p className="text-center">{this.props.blog.about}</p>
+                </div>
               </div>
             </div>
-            <div class="flip-card-back" />
-            <img
-              id="avatar"
-              src="https://stagerightshow.com/wp-content/uploads/2019/05/unique-halloween-black-cat-clipart-halloween-black-cat-png-transparent-this-year.png"
-              alt="Avatar"
-              class="rounded-circle"
-            />
-            <h6 className="font-italic">{this.props.blog.autor}</h6>
-            <p className="card-text">{this.props.blog.about}</p>
-            <Link
-              to={`/blogs/${this.props.blog.id}`}
-              className="btn btn-primary"
-            >
-              Read more
-            </Link>
+            <div className="back">
+              <div className="content">
+                <div className="main">
+                  <p className="text-center">{this.props.blog.aboutBlog}</p>
+
+                  <div className="social-links text-center">
+                    <Link to="#" className="social-icon facebook  animate">
+                      <span className="fa fa-facebook" />
+                    </Link>
+
+                    <Link
+                      to="#"
+                      target="_blank"
+                      className=" social-icon twitter  animate"
+                    >
+                      <span className="fa fa-twitter" />
+                    </Link>
+
+                    <Link
+                      to="#"
+                      target="_blank"
+                      className=" social-icon github  animate"
+                    >
+                      <span className="fa fa-github-alt" />
+                    </Link>
+                  </div>
+                  <Link
+                    to={`/blogs/${this.props.blog.id}`}
+                    className="btn btn-primary"
+                  >
+                    Read more
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/services/fakeBlogService.js
+++ src/components/services/fakeBlogService.js
@@ -3,20 +3,27 @@ export const blogs = [
     id: "1",
     autor: "Jane Doe",
     title: "C# IS AWESOME",
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    aboutBlog:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt lectus, ac consectetur dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque ultrices mi in ex tempor volutpat. Nulla elementum libero tortor, quis auctor orci euismod a. Nulla mollis et eros id blandit. Nunc vitae urna interdum diam pretium maximus. Pellentesque eget sodales urna."
   },
   {
     id: "2",
     autor: "John Smith",
     title: "My Programmer Journey",
-    about: "Praesent consequat orci ut dui semper, ut fringilla diam tincidunt."
+    about:
+      "Praesent consequat orci ut dui semper, ut fringilla diam tincidunt.",
+    aboutBlog:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt lectus, ac consectetur dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque ultrices mi in ex tempor volutpat. Nulla elementum libero tortor, quis auctor orci euismod a. Nulla mollis et eros id blandit. Nunc vitae urna interdum diam pretium maximus. Pellentesque eget sodales urna."
   },
   {
     id: "3",
     autor: "Mary Green",
     title: "How To Become A Professional Dev",
     about:
-      "Nullam posuere lorem quam. Donec est purus, feugiat vel porta in, luctus non lectus."
+      "Nullam posuere lorem quam. Donec est purus, feugiat vel porta in, luctus non lectus.",
+    aboutBlog:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis tincidunt lectus, ac consectetur dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque ultrices mi in ex tempor volutpat. Nulla elementum libero tortor, quis auctor orci euismod a. Nulla mollis et eros id blandit. Nunc vitae urna interdum diam pretium maximus. Pellentesque eget sodales urna."
   }
 ];
 
