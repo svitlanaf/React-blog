e47d5376e4a13c40c1991a445a59bc99d02e158a
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-07-23 19:26:59 -0700

    Add posts under a blog

--- src/components/blogPostList.jsx
+++ src/components/blogPostList.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import Post from "./post";
-import { getBlog } from "./services/fakeBlogService";
+import { getPosts } from "./services/fakePostService";
 
 class BlogPostList extends Component {
   state = {
-    blog: getBlog(this.props.blogId)
+    posts: []
   };
 
+  componentDidMount() {
+    this.setState({
+      posts: getPosts(this.props.match.params.id)
+    });
+  }
+
   render() {
-    return <Post blogId={this.state.blog.id} />;
+    const { length: count } = this.state.posts;
+
+    if (count === 0) return <p>There are no posts yet.</p>;
+    return (
+      <div>
+        {this.state.posts.map(p => (
+          <Post post={p} key={p.id} />
+        ))}
+      </div>
+    );
   }
 }
 
